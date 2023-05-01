2a4659d0013f81fd36c1eb0e7e36a9f5d79ef55c
yashablack <iamyashablack@gmail.com>
2023-05-01 20:46:01 +0300

    fix: update farm props

--- src/events/farm/props.ts
+++ src/events/farm/props.ts
@@ -4,12 +4,12 @@ export type FarmProps = {
   farm_id: string;
   farm_status: string;
   farm_name: string;
-  farm_x_name: string;
-  farm_y_name: string;
   farm_total_staked_x: number;
   farm_total_staked_y: number;
   farm_total_staked_usd: number;
   farm_apr: number;
+  farm_reward_asset_id: string;
+  farm_reward_asset_name: string;
 
   farm_user_staked_x: number;
   farm_user_staked_y: number;
