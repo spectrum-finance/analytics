377c56059e244da0f48dd5571d24da00d5838259
yashablack <iamyashablack@gmail.com>
2023-05-01 18:52:44 +0300

    feat: add farm events

--- src/events/farm/events.ts
+++ src/events/farm/events.ts
@@ -1,11 +1,18 @@
 import { ErrorProps } from '../generalProps';
-import { FarmsProps } from './props';
+import { FarmProps, StakeProps } from './props';
 
-export type FarmsEvents = {
-  'Farms Click Add Liquidity': FarmsProps;
-  'Farms Click Stake': FarmsProps;
-  'Farms Confirm Stake': FarmsProps;
-  'Farms Confirm Stake Error': FarmsProps & ErrorProps;
-  'Farms Cancel Stake Sign': FarmsProps;
-  'Farms Sign Stake Success': FarmsProps;
+export type FarmEvents = {
+  'Farm Click Add Liquidity': FarmProps;
+  'Farm Stake Submit': FarmProps & StakeProps;
+  'Farm Stake Confirm Modal': FarmProps & StakeProps;
+  'Farm Stake Confirm Modal Error': FarmProps & StakeProps & ErrorProps;
+  'Farm Stake Sign Success': FarmProps & StakeProps;
+  'Farm Stake Sign Error': FarmProps & StakeProps & ErrorProps;
+  'Farm Stake Cancel Sign': FarmProps & StakeProps;
+  'Farm Click Manage Add Liquidity': FarmProps;
+  'Farm Click Manage Unstake': FarmProps;
+  'Farm Unstake Confirm Modal': FarmProps & StakeProps;
+  'Farm Unstake Confirm Modal Error': FarmProps & StakeProps & ErrorProps;
+  'Farm Unstake Sign Success': FarmProps & StakeProps;
+  'Farm Unstake Sign Error': FarmProps & StakeProps & ErrorProps;
 };
