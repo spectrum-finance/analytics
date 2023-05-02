b48b8b72e5c16559c837e27ac2e05cc2ab687801
yashablack <iamyashablack@gmail.com>
2023-05-02 14:02:59 +0300

    fix: add more farm events

--- src/events/farm/events.ts
+++ src/events/farm/events.ts
@@ -3,16 +3,21 @@ import { FarmProps, StakeProps } from './props';
 
 export type FarmEvents = {
   'Farm Click Add Liquidity': FarmProps;
+  'Farm Click Stake': FarmProps;
+
   'Farm Stake Submit': FarmProps & StakeProps;
   'Farm Stake Confirm Modal': FarmProps & StakeProps;
   'Farm Stake Confirm Modal Error': FarmProps & StakeProps & ErrorProps;
   'Farm Stake Sign Success': FarmProps & StakeProps;
   'Farm Stake Sign Error': FarmProps & StakeProps & ErrorProps;
   'Farm Stake Cancel Sign': FarmProps & StakeProps;
-  'Farm Click Manage Add Liquidity': FarmProps;
-  'Farm Click Manage Unstake': FarmProps;
+
   'Farm Unstake Confirm Modal': FarmProps & StakeProps;
   'Farm Unstake Confirm Modal Error': FarmProps & StakeProps & ErrorProps;
   'Farm Unstake Sign Success': FarmProps & StakeProps;
   'Farm Unstake Sign Error': FarmProps & StakeProps & ErrorProps;
+
+  'Farm Click Manage Add Liquidity': FarmProps;
+  'Farm Click Manage Unstake': FarmProps;
+  'Farm Click Manage Stake': FarmProps;
 };
