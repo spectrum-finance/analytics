64e91f057954c62bfc94fab79a36c7db320ee84c
yashablack <iamyashablack@gmail.com>
2023-05-02 14:20:07 +0300

    fix: add unstake click event

--- src/events/farm/events.ts
+++ src/events/farm/events.ts
@@ -4,6 +4,11 @@ import { FarmProps, StakeProps } from './props';
 export type FarmEvents = {
   'Farm Click Add Liquidity': FarmProps;
   'Farm Click Stake': FarmProps;
+  'Farm Click Unstake': FarmProps;
+
+  'Farm Click Manage Add Liquidity': FarmProps;
+  'Farm Click Manage Unstake': FarmProps;
+  'Farm Click Manage Stake': FarmProps;
 
   'Farm Stake Submit': FarmProps & StakeProps;
   'Farm Stake Confirm Modal': FarmProps & StakeProps;
@@ -12,12 +17,9 @@ export type FarmEvents = {
   'Farm Stake Sign Error': FarmProps & StakeProps & ErrorProps;
   'Farm Stake Cancel Sign': FarmProps & StakeProps;
 
+  'Farm Unstake Submit': FarmProps & StakeProps;
   'Farm Unstake Confirm Modal': FarmProps & StakeProps;
   'Farm Unstake Confirm Modal Error': FarmProps & StakeProps & ErrorProps;
   'Farm Unstake Sign Success': FarmProps & StakeProps;
   'Farm Unstake Sign Error': FarmProps & StakeProps & ErrorProps;
-
-  'Farm Click Manage Add Liquidity': FarmProps;
-  'Farm Click Manage Unstake': FarmProps;
-  'Farm Click Manage Stake': FarmProps;
 };
